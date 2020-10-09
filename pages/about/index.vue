58e7778c84534e54c5277ede99c1a38d600495ac
Katarina <zain.fake02@gmail.com>
2020-10-09 23:50:48 +0200

    adding about page

---
+++ pages/about/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <NavBarUwU />
+      <div>
+        <section class="about">
+          <h5>FEW WORDS ABOUT ME</h5>
+          <div class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec volutpat augue, a ullamcorper tortor. In hac habitasse platea dictumst. Duis feugiat porttitor tellus ut fermentum. Nullam lacinia sem id.</div>
+          <div class="young">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec volutpat augue, a ullamcorper tortor. In hac habitasse platea dictumst. Duis feugiat porttitor tellus ut fermentum. Nullam lacinia sem id.</div>
+        </section>
+        
+      </div>
+      
+  </div>
+</template>
+
+<script>
+import NavBarUwU from "~/components/UI/NavBarUwU";
+
+export default {
+  components: {
+    NavBarUwU
+  },
+};
+</script>
+
+<style src="./about.scss" lang="scss" scoped />
+
