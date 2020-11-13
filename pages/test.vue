279e2a503cd033ca3a819004f91973bbe5c40275
Katarina <zain.fake02@gmail.com>
2020-11-13 22:20:18 +0100

    about mobile friendly

--- pages/test.vue
+++ pages/test.vue
@@ -4,6 +4,7 @@
     <Github />
     <Twitter />
     <Instagram />
+    <div class="banner"></div>
   </div>
 </template>
 
@@ -11,14 +12,35 @@
 import Github from "~/components/UI/Icons/Github.vue";
 import Twitter from "~/components/UI/Icons/Twitter.vue";
 import Instagram from "~/components/UI/Icons/Instagram.vue";
+import anime from "animejs";
 
 export default {
   components: {
     Github,
     Twitter,
-    Instagram
-  }
+    Instagram,
+  },
+  methods: {
+    animatesircles() {
+      anime({
+        targets: ".banner",
+        backgroundColor: "#6c6c6c",
+        easing: "linear",
+        delay: 1000,
+        duration: 1000,
+      });
+    },
+  },
+  mounted() {
+    this.animatesircles();
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.banner {
+  height: 50px;
+  width: 50px;
+  background-color: aqua;
+}
+</style>
