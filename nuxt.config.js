6fbdf95a9f32f347597227f48af09d81d9b78880
Katarina <zain.fake02@gmail.com>
2020-11-14 13:03:48 +0100

    ...

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,15 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href:
+          "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.compat.css"
+      }
+    ]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
---
+++ pages/trans.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nuxt-link tag="button" to="/about"> GO lhih</nuxt-link>
+    <h5>shkqhqskqhqksh</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  transition: {
+    name: "home",
+    mode: "out-in",
+  },
+};
+</script>
+
+<style>
+.home-enter-active,
+.home-leave-active {
+  transition: opacity 0.5s;
+}
+.home-enter,
+.home-leave-active {
+  opacity: 0;
+}
+</style>
